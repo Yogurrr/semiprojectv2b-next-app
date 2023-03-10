30c6d49dc91720ee6ef7f90fcf5763a7e0e3aa44
LeeSeungYeon <masakisqq@naver.com>
2023-03-10 17:16:18 +0900

    구글 recaptcha 테스트

--- pages/list2.js
+++ pages/list2.js
@@ -36,16 +36,16 @@ export async function getServerSideProps(ctx) {
 
     cpg = cpg ? parseInt(cpg) : 1;
     let params = `cpg=${cpg}`;   // 질의문자열 생성
-    // if(fkey) params += `&ftype=${ftype}&fkey=${encodeURIComponent(fkey)}`;
-    if(fkey) params += `&ftype=${ftype}&fkey=${fkey}`;   // axios
+    if(fkey) params += `&ftype=${ftype}&fkey=${encodeURIComponent(fkey)}`;
+    // if(fkey) params += `&ftype=${ftype}&fkey=${fkey}`;   // axios
 
     let url = `http://localhost:3000/api/board/list?${params}`;
     
-    // const res = await fetch(url);   // isomorphic-unfetch
-    // const boards = await res.json();
+    const res = await fetch(url);   // isomorphic-unfetch
+    const boards = await res.json();
 
-    const res = await axios.get(url);   // isomorphic-unfetch
-    const boards = await res.data;
+    // const res = await axios.get(url);   // isomorphic-unfetch
+    // const boards = await res.data;
 
     let alpg = Math.ceil(  parseInt(boards.allcnt) / 25);   // 총 페이지수 계산
 
@@ -87,7 +87,7 @@ export default function List( {boards} )  {
                 </colgroup>
                 <tbody>
                 <tr>
-                    <td colSpan="3" className="alignlft">
+                    <td colSpan="3" className="alignlft" style={{background: "white", textAlign: "left"}}>
                         <select name="ftype" id="ftype" onChange={handletype}>
                             <option value="title">제목</option>
                             <option value="userid">작성자</option>
--- pages/write.js
+++ pages/write.js
@@ -1,4 +1,24 @@
-const Write = () => {
+import {useState} from "react";
+
+import axios from "axios";
+
+export async function getServerSideProps(ctx) {
+    let bno = ctx.query.bno;
+
+    const url = `http://localhost:3000/api/board/view?bno=${bno}`;
+    const res = await axios.get(url);
+    const board = await res.data[0];
+    console.log(board);
+
+    return { props: {board} }
+}
+
+export default function Write ()  {
+
+    const [title, setTitle] = useState('');
+    const [userid, setUserid] = useState('yogurrr');
+    const [contents, setContents] = useState('');
+
     return (
         <div className="write">
             <h3>새글쓰기</h3>
@@ -7,7 +27,7 @@ const Write = () => {
                     <input type="text" name="title" id="title" /></div>
 
                 <div><label htmlFor="uid">작성자</label>
-                    <input type="text" name="uid" id="uid" value="yogurrr" readOnly /></div>
+                    <input type="text" name="uid" id="uid" value={userid} readOnly /></div>
 
                 <div><label htmlFor="contents" className="dragup">본문</label>
                     <textarea name="contents" id="contents" rows="7" cols="55">
@@ -21,5 +41,3 @@ const Write = () => {
         </div>
     )
 }
-
-export default Write;
