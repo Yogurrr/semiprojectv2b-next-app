b8aea494c7686e7e321ff89ee43abf335040ac77
LeeSeungYeon <masakisqq@naver.com>
2023-03-14 16:15:03 +0900

    MariaDB Member 클래스 구현 5 - NextAuth 2 - 인증절차 추가

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,9 +12,16 @@ export default NextAuth({
             password: { label: "비밀번호", type: "password" }
         },   // 로그인 폼 정의
         async authorize(credentials, req) {
-            // 아무거나 입력해도 그냥 로그인 됨
-            console.log('auth login - ', credentials);
-            return credentials;
+            // 입력한 인증 정보 가져옴
+            const email = credentials.email;
+            const passwd = credentials.passwd;
+
+            if (email === 'abc123@987xyz.com' && passwd === '987xyz') {
+                return credentials;
+            }
+            // // 아무거나 입력해도 그냥 로그인 됨
+            // console.log('auth login - ', credentials);
+            // return credentials;
         }
     })
 ]
